5344d73fce3477a8af05a4230c72c602b56a2f4b
Chang Sophia <you@example.com>
2018-09-18 13:18:55 +0800

    Initial Commit

---
+++ AnimalSaying/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalSaying
+//
+//  Created by Chang Sophia on 2018/9/18.
+//  Copyright © 2018年 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
